4b62519d6e52dd3ed6f073b180e6a6351e781d7a
aef101176 <angeloefleming76@gmail.com>
2019-11-15 15:34:05 -0500

    Update GettingStarted.java

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,3 +1,14 @@
+/*
+*  We see that:
+
+    The very first line @Test, indicates that the following method is a JUnit test. JUnit is the framework we are gonna dive into later.
+    The method name already explains what we want to test.
+    The body of this test method then invokes the function we want to test (addFive()), passing the number 20 to it.
+    Given this input, the expected output should then be 25. so we assert that our result is equal to 25 (using the Assertions.assertEquals() method). If the result is not equal to 25, we expect this test to fail.
+
+*
+* */
+
 package tudelft.gettingstarted;
 
 public class GettingStarted {
